596456755efbd52d673e814c0e357408a8279e71
Can AKYILDIRIM <akyildirimcan@gmail.com>
2024-04-17 22:23:06 +0300

    refactor: create contact app

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    
+
     (function($) {
         "use strict";
 
-    
+
     jQuery.validator.addMethod('answercheck', function (value, element) {
         return this.optional(element) || /^\bcat\b$/.test(value)
     }, "type the correct answer -_-");
@@ -58,16 +58,26 @@ $(document).ready(function(){
                 $(form).ajaxSubmit({
                     type:"POST",
                     data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
+                    url:"/contact/contact_form",
+                    success: function(response) {
+                        console.log(response);
+                        if (response.success) {
+                            $('#contactForm :input').attr('disabled', 'disabled');
+                            $('#contactForm').fadeTo( "slow", 1, function() {
+                                $(this).find(':input').attr('disabled', 'disabled');
+                                $(this).find('label').css('cursor','default');
+                                $('#success').fadeIn()
+                                $('.modal').modal('hide');
+                                $('#success').modal('show');
+                            })
+                        } else {
+                            $('#contactForm').fadeTo( "slow", 1, function() {
+                                $('#error').fadeIn()
+                                $('.modal').modal('hide');
+                                $('#error').modal('show');
+                            })
+                        }
+
                     },
                     error: function() {
                         $('#contactForm').fadeTo( "slow", 1, function() {
@@ -80,6 +90,6 @@ $(document).ready(function(){
             }
         })
     })
-        
+
  })(jQuery)
 })
