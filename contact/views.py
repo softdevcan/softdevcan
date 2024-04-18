56ac3191e2e9dd080de271448f4a3d07788993b0
Can AKYILDIRIM <akyildirimcan@gmail.com>
2024-04-18 17:21:20 +0300

    feat: create forms and send email

--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from contact.models import Message
+from contact.forms import ContactForm
 
 
 # Create your views here.
 
 def contact_form(request):
+    if request.POST:
+        contact_form = ContactForm(request.POST or None)
+        if contact_form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
+
+            Message.objects.create(
+                name=name,
+                email=email,
+                subject=subject,
+                message=message,
+            )
+
+            contact_form.send_mail()
+
+            success = True
+            message = 'Contact form sent successfully.'
+        else:
+            success = False
+            message = 'Contact form is not valid.'
+    else:
+        success = False
+        message = 'Request method is not valid.'
+
     context = {
-        'success': True,
-        'message': 'Contact form sent successfully.',
+        'success': success,
+        'message': message,
     }
     return JsonResponse(context)
 
+
 def contact(request):
-    return render(request, 'contact.html')
+    contact_form = ContactForm()
+    context = {
+        'contact_form': contact_form,
+    }
+    return render(request, 'contact.html', context)
--- resume/settings.py
+++ resume/settings.py
@@ -133,3 +133,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+e = env('EMAIL')
+p = env('EMAIL_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = str(e)
+EMAIL_HOST_PASSWORD = str(p)
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
