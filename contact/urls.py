56a42a051957c23e58964e2dfff896666e4bb809
Can AKYILDIRIM <akyildirimcan@gmail.com>
2024-04-30 11:40:31 +0300

    refactor: updates urls

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,5 @@ from .views import contact_form, contact
 
 urlpatterns = [
     path('contact_form', contact_form, name='contact_form'),
-    path('contact', contact, name='contact'),
+    path('', contact, name='contact'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from .views import index, redirect_urls
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', include('contact.urls')),
     path('blog/', include('blog.urls')),
     path('<slug>/', redirect_urls, name='redirect_urls'),
 ]
